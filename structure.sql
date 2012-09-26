abbaf51cc29e92d63b8cfa2016aac6009b62a57a
Wilcley <wilcley@kladium.com>
2012-09-26 18:27:27 -0400

    added structure.sql with commands for setting up the database

---
+++ structure.sql
@@ -0,0 +1,64 @@
+/*
+SQLyog Community v10.3 
+MySQL - 5.5.9 : Database - perkleit
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`perkleit` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `perkleit`;
+
+/*Table structure for table `map` */
+
+CREATE TABLE `map` (
+  `map_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `max_moves` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`map_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `player` */
+
+CREATE TABLE `player` (
+  `player_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `player_hash` char(43) COLLATE utf8_unicode_ci NOT NULL,
+  `map_id` int(10) unsigned NOT NULL,
+  `moves_remaining` int(11) NOT NULL,
+  `x` int(11) DEFAULT NULL,
+  `y` int(11) DEFAULT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`player_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tile` */
+
+CREATE TABLE `tile` (
+  `tile_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `map_id` int(10) unsigned NOT NULL,
+  `tile_type_id` int(10) unsigned NOT NULL,
+  `x` smallint(5) unsigned NOT NULL,
+  `y` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`tile_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `tile_types` */
+
+CREATE TABLE `tile_types` (
+  `tile_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `open` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `special` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `boster` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`tile_type_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
