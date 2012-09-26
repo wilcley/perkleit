ce7ae906469f19f6cacbd8d6c1a18c70046b4c14
Wilcley <wilcley@kladium.com>
2012-09-26 18:05:28 -0400

    cleanup test files

--- hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type: text/plain");
-
-for ($i = 0; $i<10; $i++) {
-    $hash = base64_encode(hash('sha256',mt_rand(),true));
-    echo $hash . " " . strlen($hash) . "\n";
-}
-?>
